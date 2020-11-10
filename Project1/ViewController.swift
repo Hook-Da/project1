70f854a48819479a1c5c33dd8dfa49c7a2612bc3
Hook-Da <timur-banner@rambler.ru>
2020-11-10 22:41:07 +0500

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Hook Banner on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
